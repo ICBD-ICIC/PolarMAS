ea01cce4c08dffabd12975ec4d9a98b46a778f16
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2025-03-17 11:17:21 +0100

    Notes post meeting

--- langchain_example.py
+++ langchain_example.py
@@ -6,7 +6,7 @@ import os
 
 #PARAMETERS
 topic = "climate change"
-context = "Environmental Protection Agency (EPA) issued an extraordinary cavalcade of pollution rule rollback."
+context = "Environmental Protection Agency (EPA) issued an extraordinary cavalcade of pollution rule rollback"
 number_messages = 10
 #neutral_discuss = False
 
@@ -29,15 +29,22 @@ Reply to the four questions following the format <question identifier>: <value>.
 # Create agents
 class Agent:
     def __init__(self, name, role):
-        self.name = name
+        self.name = name # we can remove the name
         self.role = role
         self.memory = []
 
     def respond(self, message):
-        self.memory.append(HumanMessage(content=message))
+        self.memory.append(HumanMessage(content=message)) # need to replace the old conversation
+        #create and id for the conversation and replace if already exists
         response = llm([SystemMessage(content=f"You are {self.name}, {self.role}."), *self.memory])
+        response.content = f'{self.name}: {response.content}' #see why it duplicates the name
         self.memory.append(response)
+        print(self.memory)
         return response.content
+
+   # def observe(self, message):
+        # just updates the memory, without responding
+        # we need to call observe at the end of the conversation, for everyone so the "stair issue" is not a problem
 
 # Instantiate agents
 agent1 = Agent("Agent1", "Moderate democrat")
@@ -52,18 +59,23 @@ def facilitate_discussion(discussion_prompt):
     print("-- Initial Questionnaire --")
     for agent in all_agents:
         print(f"{agent.name}'s response: {agent.respond(QUESTIONNAIRE)}\n")
+        #agent.respond(QUESTIONNAIRE)
 
     print("-- Discussion Begins --")
     messages = [discussion_prompt]
-    for _ in range(10):
-        agent = random.choice(discussing_agents)
-        response = agent.respond(messages)
-        print(f"{agent.name}: {response}\n")
-        messages.append(response)
+    # for _ in range(10):
+    #     agent = random.choice(discussing_agents)
+    for i in range(2):
+        for agent in discussing_agents:
+            response = agent.respond(messages)
+            #print(f"{agent.name}: {response}\n")
+            messages.append(response)
 
     print("-- Post Discussion Questionnaire --")
     for agent in all_agents:
-        print(f"{agent.name}'s response: {agent.respond(QUESTIONNAIRE)}\n")
+        print(f"{agent.name}'s response: {agent.respond(QUESTIONNAIRE)}\n") # if it does not interact, it does not have the memory
+        #agent.respond(QUESTIONNAIRE)
 
 # Example run
 facilitate_discussion(f'Under the context of {context}. Discuss on {topic}. Limit your speak up to 100 words.')
+# this prompt should not go on the observers memory
