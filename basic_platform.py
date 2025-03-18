1f378513f12a158bc0311668cc725c44c1bf25ea
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2025-03-18 17:49:38 +0100

    Inputs and Outputs with files

--- basic_platform.py
+++ basic_platform.py
@@ -1,15 +1,21 @@
 import pprint
-import random
+from datetime import datetime
 
+import pandas as pd
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
 import os
+import json
 
 #PARAMETERS
 topic = "climate change"
 context = "Environmental Protection Agency (EPA) issued an extraordinary cavalcade of pollution rule rollback"
-number_messages = 10
-#neutral_discuss = False
+number_messages_per_agent = 1
+#define range of adjectives.
+SCENARIO_NAME = '1D_1R_RR1'
+SCENARIO_CONFIG = pd.read_csv(f'scenarios/{SCENARIO_NAME}.csv')
+
+output_data = {'pre_questionare': {}, 'post_questionare': {}, 'discussion': []}
 
 # Set up your Google API key
 os.environ["GOOGLE_API_KEY"] = os.environ["GEMINI_API_KEY"]
@@ -40,8 +46,6 @@ PRE_QUESTIONNAIRE_ID = 0
 POST_QUESTIONNAIRE_ID = 2
 CONVERSATION_ID = 1
 
-
-#TBD: get 3 different classes for each type, and parametrize the moderate/polarized/extreme etc
 # Create agents
 class Agent:
     def __init__(self, name, role):
@@ -59,40 +63,49 @@ class Agent:
     def observe(self, message, conversation_id):
         self.memory[conversation_id] = [HumanMessage(content=message)]
 
+all_agents = []
+discussing_agents = []
 
-# Instantiate agents
-agent1 = Agent("Agent1", "Moderate democrat")
-agent2 = Agent("Agent2", "Extreme republican")
-agent3 = Agent("Agent3", "Neutral about democrats and republicans")
-
-all_agents = [agent1, agent2, agent3]
-discussing_agents = [agent1, agent2, agent3]  #TBD remove neutrals if parameter set to true
+for agent_config in SCENARIO_CONFIG.itertuples():
+    for amount in range(agent_config.amount):
+        agent = Agent(f'Agent{len(all_agents)}', agent_config.role)
+        all_agents.append(agent)
+        if agent_config.observer != 'True':
+            discussing_agents.append(agent)
 
 
 # Facilitator to manage discussion
 def facilitate_discussion(discussion_prompt):
     print("-- Initial Questionnaire --")
     for agent in all_agents:
-        print(f"{agent.name}'s response: {agent.respond(PRE_QUESTIONNAIRE, PRE_QUESTIONNAIRE_ID)}\n")
+        response = agent.respond(PRE_QUESTIONNAIRE, PRE_QUESTIONNAIRE_ID)
+        output_data['pre_questionare'][agent.name] = response
+        print(f"{agent.name}'s response: "
+              f"{response}\n")
 
     print("-- Discussion Begins --")
     messages = [discussion_prompt]
     # for _ in range(10):
     #     agent = random.choice(discussing_agents)
-    for i in range(2):
+    for i in range(number_messages_per_agent):
         for agent in discussing_agents:
             response = agent.respond(messages, CONVERSATION_ID)
-            #print(f"{agent.name}: {response}\n")
+            print(f"{agent.name}: {response}\n")
             messages.append(response)
 
     print("-- Post Discussion Questionnaire --")
     for agent in all_agents:
         agent.observe(messages, CONVERSATION_ID)
-        print(f"{agent.name}'s response: {agent.respond(POST_QUESTIONNAIRE, POST_QUESTIONNAIRE_ID)}\n")
-        #agent.respond(QUESTIONNAIRE)
-
+        output_data['discussion'] = messages
+        response = agent.respond(POST_QUESTIONNAIRE, POST_QUESTIONNAIRE_ID)
+        output_data['post_questionare'][agent.name] = response
+        print(f"{agent.name}'s response:"
+              f" {response}\n")
 
 # Example run
 facilitate_discussion(
     f'Under the context of {context}. Discuss on {topic}. Limit your speak up to 100 words. Everytime you respond, respond with <name>:.')
-# this prompt should not go on the observers memory
+
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+with open(f'outputs/{SCENARIO_NAME}_{timestamp}.json', 'w') as fp:
+    json.dump(output_data, fp)
