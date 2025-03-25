0325e9e2d882bbc44647bcd52e0f42c9fe83109c
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2025-03-25 19:26:39 +0100

    Add demographics to agent

--- basic_platform.py
+++ basic_platform.py
@@ -10,7 +10,8 @@ import json
 topic = "climate change"
 context = "Environmental Protection Agency (EPA) issued an extraordinary cavalcade of pollution rule rollback"
 number_messages_per_agent = 1
-SCENARIO_NAME = '1ED_1ER_RR1'
+#SCENARIO_NAME = '1MD_1MR_1N_RR1'
+SCENARIO_NAME = '1ED_1MR_RR1'
 SCENARIO_CONFIG = pd.read_csv(f'scenarios/{SCENARIO_NAME}.csv')
 
 output_data = {'pre_questionnaire': {}, 'post_questionnaire': {}, 'discussion': []}
@@ -18,6 +19,13 @@ output_data = {'pre_questionnaire': {}, 'post_questionnaire': {}, 'discussion': 
 os.environ["GOOGLE_API_KEY"] = os.environ["GEMINI_API_KEY"]
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7)
+
+# AGENT_KNOWLEDGE = """
+# An Extreme Democrat has an unwavering support for the Democratic Party and extreme hatred towards the Republican Party.
+# An Extreme Republican has an unwavering support for the Republican Party and extreme hatred towards the Democratic Party.
+# """
+
+AGENT_KNOWLEDGE = ""
 
 PRE_QUESTIONNAIRE = """
 Q1. On a scale from 0 to 10, where 0 represents no support at all and 10 signifies unwavering support, how would you rate your level of support for the Republican Party?
@@ -36,20 +44,23 @@ Q4. On a scale from 0 to 10, where 0 means no dislike at all and 10 represents e
 Reply to the four questions following the format <question identifier>: <value>. Just give the answer.
 """
 
-PRE_QUESTIONNAIRE_ID = 0
-POST_QUESTIONNAIRE_ID = 2
-CONVERSATION_ID = 1
+memory_order = ['agent_knowledge',
+                'pre_questionnaire',
+                'discussion',
+                'post_questionnaire']
 
 class Agent:
-    def __init__(self, name, role, persona):
+    def __init__(self, name, role, persona, demographics):
         self.name = name
         self.role = role
         self.persona = persona
+        self.demographics = demographics
         self.memory = {}
 
     def respond(self, message, conversation_id):
         self.memory[conversation_id] = [HumanMessage(content=message)]
-        response = llm([SystemMessage(content=f"You are {self.name}, {self.role}. {self.persona}"), *sum(self.memory.values(), [])])
+        response = llm([SystemMessage(content=f"You are {self.name}, {self.role}.{self.demographics} {self.persona}"),
+                        *sum(self.memory.values(), [])])
         self.memory[conversation_id].append(response)
         return response.content
 
@@ -60,9 +71,10 @@ all_agents = []
 discussing_agents = []
 
 for agent_config in SCENARIO_CONFIG.itertuples():
-    agent = Agent(f'Agent{len(all_agents)}', agent_config.role, agent_config.persona)
+    agent = Agent(f'Agent{len(all_agents)}', agent_config.role, agent_config.persona, agent_config.demographics)
+    agent.observe(AGENT_KNOWLEDGE, memory_order.index('agent_knowledge'))
     all_agents.append(agent)
-    if agent_config.observer == False:
+    if agent_config.is_observer == False:
         discussing_agents.append(agent)
 
 
@@ -70,7 +82,7 @@ for agent_config in SCENARIO_CONFIG.itertuples():
 def facilitate_discussion(discussion_prompt):
     print("-- Initial Questionnaire --")
     for agent in all_agents:
-        response = agent.respond(PRE_QUESTIONNAIRE, PRE_QUESTIONNAIRE_ID)
+        response = agent.respond(PRE_QUESTIONNAIRE, memory_order.index('pre_questionnaire'))
         output_data['pre_questionnaire'][agent.name] = response
         print(f'- {agent.name} - {agent.role} -')
         print(response)
@@ -79,15 +91,15 @@ def facilitate_discussion(discussion_prompt):
     messages = [discussion_prompt]
     for i in range(number_messages_per_agent):
         for agent in discussing_agents:
-            response = agent.respond(messages, CONVERSATION_ID)
+            response = agent.respond(messages, memory_order.index('discussion'))
             print(response)
             messages.append(response)
 
     print("-- Post Discussion Questionnaire --")
     for agent in all_agents:
-        agent.observe(messages, CONVERSATION_ID)
+        agent.observe(messages, memory_order.index('discussion'))
         output_data['discussion'] = messages
-        response = agent.respond(POST_QUESTIONNAIRE, POST_QUESTIONNAIRE_ID)
+        response = agent.respond(POST_QUESTIONNAIRE, memory_order.index('post_questionnaire'))
         output_data['post_questionnaire'][agent.name] = response
         print(f'- {agent.name} - {agent.role} -')
         print(response)
