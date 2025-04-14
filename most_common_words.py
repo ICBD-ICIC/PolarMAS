986f3f77bdb8aea8cbf636289bfea80e628335fc
StephanieMalvicini <s.a.m_puan@hotmail.com>
2025-04-14 19:03:25 +0200

    most common words

--- most_common_words.py
+++ most_common_words.py
@@ -4,24 +4,16 @@ from collections import Counter
 import re
 from nltk.corpus import stopwords
 
-# Path to the folder containing the JSON files
-folder_path = "outputs/h1_non_political_reverse"
-stop_words = set(stopwords.words('english'))
+experiment_type = 'h1_non_political'
 
-# Intro message to remove
-intro_message = (
-    "\nWe've randomly assigned you a partner that belongs to or leans toward your out party. "
-    "Please have a conversation by chatting with them about the meaning of life.\nSpecifically, we are interested in you sharing "
-    "what you think makes life meaningful and learning your conversation partner's thoughts as someone that might hold different values and beliefs.\n"
-    "For example, survey research shows that many people mention family as the most important sources of meaning in their life. Survey research also shows "
-    "that other people mention career, money, faith, friends, and hobbies as the most important source of meaning in their life. \n"
-    "What do you think?. Limit your words to 50.\nEverytime you respond, respond with <name>:."
-)
+# Path to the folder containing the JSON files
+folder_path = f"outputs/{experiment_type}"
+stop_words = set(stopwords.words('english'))
 
 # Function to clean and tokenize text
 def tokenize(text):
     words = re.findall(r'\b\w+\b', text.lower())
-    return  [word for word in words if word not in stop_words]
+    return  [word for word in words if word not in stop_words and 'agent' not in word]
 
 # Counter for all words
 word_counter = Counter()
@@ -32,15 +24,13 @@ for filename in os.listdir(folder_path):
         filepath = os.path.join(folder_path, filename)
         with open(filepath, "r", encoding="utf-8") as f:
             data = json.load(f)
-            discussion = data.get("discussion", [])
+            discussion = data.get("discussion", [])[1:-1]
             for entry in discussion:
-                if entry.strip() == intro_message.strip():
-                    continue  # Skip the intro message
                 words = tokenize(entry)
                 word_counter.update(words)
 
 # Get the most common words
-most_common_words = word_counter.most_common(50)  # Top 50
+most_common_words = word_counter.most_common(20)
 
 # Print the results
 for word, count in most_common_words:
